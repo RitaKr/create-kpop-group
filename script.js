394b172eeed698fd2431e4c778b2acad8a31e1d1
RitaKr <ritokra@gmail.com>
2022-04-15 23:45:22 +0200

    more options

--- script.js
+++ script.js
@@ -9,10 +9,13 @@ let gameArr = [
         cards: [
             '<img src="images/1/yg.jpg" class="background-img" width="100%"><span class="cilent-table">YG Entertainment</span>',
             '<img src="images/1/cube.png" class="background-img" width="100%"><span class="cilent-table">Cube Entertainment</span>',
-            '<img src="images/1/bh.jpg" class="background-img" width="100%"><span class="cilent-table">BigHit Entertainment</span>',
+            '<img src="images/1/hybe.jpg" class="background-img"><span class="cilent-table">HYBE Labels</span>',
             '<img src="images/1/jyp.jpg" class="background-img" width="100%"><span class="cilent-table">JYP Entertainment</span>',
-            '<img src="images/1/pledis.png" class="background-img" width="100%"><span class="cilent-table">Pledis Entertainment</span>',
-            '<img src="images/1/sm.jpg" class="background-img" width="100%"><span class="cilent-table">SM Entertainment</span>'
+            '<img src="images/1/sm.jpg" class="background-img" width="100%"><span class="cilent-table">SM Entertainment</span>',
+            '<img src="images/1/pnation.jpg" class="background-img"><span class="cilent-table">P-Nation</span>',
+            '<img src="images/1/woollim.jpg" class="background-img"><span class="cilent-table">Woollim Entertainment</span>',
+            '<img src="images/1/fnc.jpg" class="background-img"><span class="cilent-table">FNC Entertainment</span>',
+            '<img src="images/1/kq.jpg" class="background-img"><span class="cilent-table">KQ Entertainment</span>'
         ]
     },
     {
@@ -22,8 +25,12 @@ let gameArr = [
         cards: [
             '4 мембера',
             '5 мемберов',
+            '6 мемберов',
             '7 мемберов',
+            '8 мемберов',
             '9 мемберов',
+            '10 мемберов',
+            '12 мемберов',
             '11 мемберов',
             '13 мемберов'
             ]
@@ -38,7 +45,13 @@ let gameArr = [
             '<img class="background-img" src="images/3/sad-concept.jpg" title="драматический"><span class="answer-title">драматический</span>',
             '<img class="background-img" src="images/3/fantasy-concept.jpg" title="фантастический"><span class="answer-title">фантастический</span>',
             '<img class="background-img" src="images/3/school-concept.png" title="школьный"><span class="answer-title">школьный</span>',
-            '<img class="background-img" src="images/3/aesthetic-concept.jpg" title="эстетичный"><span class="answer-title">эстетичный</span>'
+            '<img class="background-img" src="images/3/aesthetic-concept.jpg" title="эстетичный"><span class="answer-title">эстетичный</span>',
+            '<img class="background-img" src="images/3/korea.jpg" title="корейский етнический"><span class="answer-title">корейский етнический</span>',
+            '<img class="background-img" src="images/3/sport.jpg" title="спортивный"><span class="answer-title">спортивный</span>',
+            '<img class="background-img" src="images/3/soft.jpg" title="уютный"><span class="answer-title">уютный</span>',
+            '<img class="background-img" src="images/3/sci-fi.png" title="футуристический"><span class="answer-title">футуристический</span>',
+            '<img class="background-img" src="images/3/summer.jpg" title="летний"><span class="answer-title">летний</span>',
+            '<img class="background-img" src="images/3/vampire.jpg" title="вампирский"><span class="answer-title">вампирский</span>'
             ]
     },
     {
@@ -51,7 +64,10 @@ let gameArr = [
             '<img class="background-img" src="images/4/gdragon.jpg" title="BigBang"><span class="answer-title">G-Dragon</span>',
             '<img class="background-img" src="images/4/suho.jpg" title="EXO"><span class="answer-title">Сухо</span>',
             '<img class="background-img" src="images/4/shownu.jpg" title="Monsta X"><span class="answer-title">Шону</span>',
-            '<img class="background-img" src="images/4/jb.jpg" title="Got7"><span class="answer-title">JB</span>'
+            '<img class="background-img" src="images/4/jb.jpg" title="ex Got7"><span class="answer-title">JB</span>',
+            '<img class="background-img" src="images/4/taeyoung.jpg" title="NCT"><span class="answer-title">Тэён</span>',
+            '<img class="background-img" src="images/4/jungwon.jpg" title="ENHYPEN"><span class="answer-title">Чонвон</span>',
+            '<img class="background-img" src="images/4/soobin.jpg" title="TXT"><span class="answer-title">Субин</span>'
             ]
     },
     {
@@ -64,7 +80,11 @@ let gameArr = [
             '<img class="background-img" src="images/5/top.png" title="BigBang"><span class="answer-title">T.O.P</span>',
             '<img class="background-img" src="images/5/zico.jpg" title="Block B"><span class="answer-title">Zico</span>',
             '<img class="background-img" src="images/5/jooheon.jpg" title="Monsta X"><span class="answer-title">Джухон</span>',
-            '<img class="background-img" src="images/5/mark.jpg" title="NCT 127, NCT U, SuperM"><span class="answer-title">Марк</span>'
+            '<img class="background-img" src="images/5/mark.jpg" title="NCT, SuperM"><span class="answer-title">Марк</span>',
+            '<img class="background-img" src="images/5/chanyeol.jpg" title="EXO"><span class="answer-title">Чанёль</span>',
+            '<img class="background-img" src="images/5/bobby.jpg" title="IKON"><span class="answer-title">Бобби</span>',
+            '<img class="background-img" src="images/5/hongjoong.jpg" title="ATEEZ"><span class="answer-title">Хонджун</span>',
+            '<img class="background-img" src="images/5/mino.jpg" title="WINNER"><span class="answer-title">Мино</span>',
             ]
     },
     {
@@ -72,12 +92,16 @@ let gameArr = [
         roundId: 6,
         aviable: true,
         cards: [
-            '<img class="background-img" src="images/6/chanyeol.jpg" title="EXO"><span class="answer-title">Чанёль</span>',
-            '<img class="background-img" src="images/6/bobby.jpg" title="IKON"><span class="answer-title">Бобби</span>',
+            
             '<img class="background-img" src="images/6/yeonjun.jpg" title="TXT"><span class="answer-title">Ёнджун</span>',
             '<img class="background-img" src="images/6/han.png" title="Stray Kids"><span class="answer-title">Хан</span>',
-            '<img class="background-img" src="images/6/hongjoong.jpg" title="ATEEZ"><span class="answer-title">Хонджун</span>',
-            '<img class="background-img" src="images/6/jackson.jpg" title="Got7"><span class="answer-title">Джексон</span>'
+            '<img class="background-img" src="images/6/jackson.jpg" title="ex Got7"><span class="answer-title">Джексон</span>',
+            '<img class="background-img" src="images/6/mingi.jpg" title="ATEEZ"><span class="answer-title">Минги</span>',
+            '<img class="background-img" src="images/6/sehun.jpg" title="EXO"><span class="answer-title">Сехун</span>',
+            '<img class="background-img" src="images/6/vernon.jpg" title="SEVENTEEN"><span class="answer-title">Вернон</span>',
+            '<img class="background-img" src="images/6/wooseok.jpg" title="Pentagon"><span class="answer-title">Усок</span>',
+            '<img class="background-img" src="images/6/hyunsuk.jpg" title="Treasure"><span class="answer-title">Хёнсок</span>',
+            '<img class="background-img" src="images/6/sonwoo.jpg" title="The Boyz"><span class="answer-title">Сону</span>'
             ]
     },
     {
@@ -90,7 +114,11 @@ let gameArr = [
             '<img class="background-img" src="images/7/kihyun.jpg" title="Monsta X"><span class="answer-title">Кихён</span>',
             '<img class="background-img" src="images/7/seungmin.jpg" title="Stray Kids"><span class="answer-title">Сынмин</span>',
             '<img class="background-img" src="images/7/taeyang.jpg" title="BigBang"><span class="answer-title">Тэян</span>',
-            '<img class="background-img" src="images/7/jongho.png" title="ATEEZ"><span class="answer-title">Чонхо</span>'
+            '<img class="background-img" src="images/7/jongho.png" title="ATEEZ"><span class="answer-title">Чонхо</span>',
+            '<img class="background-img" src="images/7/jungkook.jpg" title="BTS"><span class="answer-title">Чонгук</span>',
+            '<img class="background-img" src="images/7/heeseung.jpg" title="ENHYPEN"><span class="answer-title">Хисын</span>',
+            '<img class="background-img" src="images/7/dk.jpg" title="SEVENTEEN"><span class="answer-title">DK</span>',
+            '<img class="background-img" src="images/7/seoho.jpg" title="Oneus"><span class="answer-title">Сохо</span>',
             ]
     },
     {
@@ -98,12 +126,16 @@ let gameArr = [
         roundId: 8,
         aviable: true,
         cards: [
-            '<img class="background-img" src="images/8/jaehyun.jpg" title="NCT 127, NCT U"><span class="answer-title">Джэхён</span>',
+            '<img class="background-img" src="images/8/jaehyun.jpg" title="NCT"><span class="answer-title">Джэхён</span>',
             '<img class="background-img" src="images/8/chen.jpg" title="EXO"><span class="answer-title">Чен</span>',
             '<img class="background-img" src="images/8/jimin.jpg" title="BTS"><span class="answer-title">Чимин</span>',
             '<img class="background-img" src="images/8/taehyun.jpg" title="TXT"><span class="answer-title">Тэхён</span>',
             '<img class="background-img" src="images/8/hui.jpg" title="Pentagon"><span class="answer-title">Хуи</span>',
-            '<img class="background-img" src="images/8/jae.jpg" title="Day6"><span class="answer-title">Джэ</span>'
+            '<img class="background-img" src="images/8/jae.jpg" title="ex Day6"><span class="answer-title">Джэ</span>',
+            '<img class="background-img" src="images/8/youngjae.jpg" title="ex Got7"><span class="answer-title">Ёнджэ</span>',
+            '<img class="background-img" src="images/8/yedam.jpg" title="Treasure"><span class="answer-title">Йедам</span>',
+            '<img class="background-img" src="images/8/woozi.jpg" title="SEVENTEEN"><span class="answer-title">Woozi</span>',
+            '<img class="background-img" src="images/8/san.jpg" title="ATEEZ"><span class="answer-title">Сан</span>'
             ]
     },
     {
@@ -115,8 +147,11 @@ let gameArr = [
             '<img class="background-img" src="images/9/taemin.jpg" title="SHINEe, SuperM"><span class="answer-title">Тэмин</span>',
             '<img class="background-img" src="images/9/leeknow.jpg" title="Stray Kids"><span class="answer-title">Lee know</span>',
             '<img class="background-img" src="images/9/kai.jpg" title="EXO, SuperM"><span class="answer-title">Кай</span>',
-            '<img class="background-img" src="images/9/dino.jpg" title="Seventeen"><span class="answer-title">Дино</span>',
-            '<img class="background-img" src="images/9/bambam.jpg" title="Got7"><span class="answer-title">Бэм-бэм</span>'
+            '<img class="background-img" src="images/9/hoshi.jpg" title="SEVENTEEN"><span class="answer-title">Хоши</span>',
+            '<img class="background-img" src="images/9/bambam.jpg" title="ex Got7"><span class="answer-title">Бэм-бэм</span>',
+            '<img class="background-img" src="images/9/niki.jpg" title="ENHYPEN"><span class="answer-title">Ники</span>',
+            '<img class="background-img" src="images/9/ten.jpg" title="NCT, WayV, SuperM"><span class="answer-title">Тэн</span>',
+            '<img class="background-img" src="images/9/q.jpg" title="The Boyz"><span class="answer-title">Q</span>'
             ]
     },
     {
@@ -127,9 +162,12 @@ let gameArr = [
             '<img class="background-img" src="images/10/hyunjin.jpg" title="Stray Kids"><span class="answer-title">Хёнджин</span>',
             '<img class="background-img" src="images/10/jin.jpg" title="BTS"><span class="answer-title">Джин</span>',
             '<img class="background-img" src="images/10/eunwoo.jpg" title="Astro"><span class="answer-title">Ыну</span>',
-            '<img class="background-img" src="images/10/lucas.jpg" title="NCT U, WayV, SuperM"><span class="answer-title">Лукас</span>',
-            '<img class="background-img" src="images/10/jinyoung.jpg" title="Got7"><span class="answer-title">Джинён</span>',
-            '<img class="background-img" src="images/10/jinwoo.jpg" title="Winner"><span class="answer-title">Джину</span>'
+            '<img class="background-img" src="images/10/lucas.jpg" title="NCT, WayV, SuperM"><span class="answer-title">Лукас</span>',
+            '<img class="background-img" src="images/10/jinyoung.jpg" title="ex Got7"><span class="answer-title">Джинён</span>',
+            '<img class="background-img" src="images/10/jinwoo.jpg" title="Winner"><span class="answer-title">Джину</span>',
+            '<img class="background-img" src="images/10/bomgyu.jpg" title="TXT"><span class="answer-title">Бомгю</span>',
+            '<img class="background-img" src="images/10/sunghoon.jpg" title="ENHYPEN"><span class="answer-title">Сонхун</span>',
+            '<img class="background-img" src="images/10/hyunjae.jpg" title="The Boyz"><span class="answer-title">Хёнджэ</span>'
             ]
     },
     {
@@ -137,12 +175,15 @@ let gameArr = [
         roundId: 11,
         aviable: true,
         cards: [
-            '<img class="background-img" src="images/11/jungkook.jpg" title="BTS"><span class="answer-title">Чонгук</span>',
-            '<img class="background-img" src="images/11/sehun.jpg" title="EXO"><span class="answer-title">Сехун</span>',
+            
+            '<img class="background-img" src="images/11/junghwan.jpg" title="Treasure" width="100%"><span class="answer-title">Джонхван</span>',
             '<img class="background-img" src="images/11/jisung.jpg" title="NCT Dream"><span class="answer-title">Джисон</span>',
             '<img class="background-img" src="images/11/in.jpg" title="Stray Kids"><span class="answer-title">I.N</span>',
             '<img class="background-img" src="images/11/kyuhyun.jpg" title="Super Junior"><span class="answer-title">Кюхён</span>',
-            '<img class="background-img" src="images/11/yugyeom.jpg" title="Got7"><span class="answer-title">Югём</span>'
+            '<img class="background-img" src="images/11/yugyeom.jpg" title="ex Got7"><span class="answer-title">Югём</span>',
+            '<img class="background-img" src="images/11/eric.jpg" title="The Boyz"><span class="answer-title">Эрик</span>',
+            '<img class="background-img" src="images/11/im.jpg" title="Monsta X"><span class="answer-title">I.M</span>',
+            '<img class="background-img" src="images/11/dino.jpg" title="SEVENTEEN"><span class="answer-title">Дино</span>'
             ]
     },
     {
@@ -153,9 +194,11 @@ let gameArr = [
             '<img class="background-img" src="images/12/yuto.jpg" title="Юто (Pentagon)"><span class="answer-title">Японец</span>',
             '<img class="background-img" src="images/12/lay.jpg" title="Лэй (EXO)"><span class="answer-title">Китаец</span>',
             '<img class="background-img" src="images/12/ten.jpg" title="Тэн (NCT U, WayV, SuperM)"><span class="answer-title">Таец</span>',
-            '<img class="background-img" src="images/12/vernon.jpg" title="Вернон (Seventeen)"><span class="answer-title">Американец</span>',
+            '<img class="background-img" src="images/12/jay.jpg" title="Джей (ENHYPEN)"><span class="answer-title">Американец</span>',
             '<img class="background-img" src="images/12/felix.jpg" title="Феликс (Stray Kids)"><span class="answer-title">Австралиец</span>',
-            'Нет иностранных участников'
+            'Нет иностранных участников',
+            '<img class="background-img" src="images/12/kevin.jpg" title="Кевин (The Boyz)"><span class="answer-title">Канадец</span>',
+            '<img class="background-img" src="images/12/alex.jpg" title="Алекс (Drippin)"><span class="answer-title">Немец</span>'
             ]
     },
     {
@@ -168,7 +211,10 @@ let gameArr = [
             '27 июня',
             '30 сентября',
             '21 августа',
-            '8 апреля'
+            '8 апреля',
+            '4 марта',
+            '20 ноября',
+            '16 мая'
             ]
     },
     {
@@ -181,7 +227,8 @@ let gameArr = [
             '2 года',
             '3 года',
             '5 лет',
-            '7 лет'
+            '7 лет',
+            '10 лет'
             ]
     },
     {
@@ -189,12 +236,14 @@ let gameArr = [
         roundId: 15,
         aviable: true,
         cards: [
-            '<img class="background-img" src="images/15/theshow.jpg" width="100%" ><span class="cilent-table">TheShow</span>',
-            '<img class="background-img" src="images/15/mcountdown.jpg" ><span class="cilent-table">MCOUNTDOWN</span>',
+            '<img class="background-img" src="images/15/theshow.jpg" width="100%" ><span class="cilent-table">The Show</span>',
+            '<img class="background-img" src="images/15/mcountdown.jpg" ><span class="cilent-table">M Countdown</span>',
             '<img class="background-img" src="images/15/inkigayo.jpg" ><span class="cilent-table">Inkigayo</span>',
             '<img class="background-img" src="images/15/k-chart.jpg" width="100%" ><span class="cilent-table">K-chart</span>',
-            '<img class="background-img" src="images/15/show-champion.jpg" width="100%" ><span class="cilent-table">Show-champion</span>',
-            '<img class="background-img" src="images/15/umt.jpg" title="УКРАЇНА МАЄ ТАЛАНТ)0)))"><span class="cilent-table">УКРАЇНА МАЄ ТАЛАНТ)0)))</span>'
+            '<img class="background-img" src="images/15/show-champion.jpg" width="100%" ><span class="cilent-table">Show Champion</span>',
+            '<img class="background-img" src="images/15/umt.jpg" title="УКРАЇНА МАЄ ТАЛАНТ)0)))"><span class="cilent-table">УКРАЇНА МАЄ ТАЛАНТ)0)))</span>',
+            '<img class="background-img" src="images/15/music-bank.jpg" heigh="100%" ><span class="cilent-table">Music Bank</span>',
+            '<img class="background-img" src="images/15/show-music-core.jpg" width="100%" ><span class="cilent-table">Show! Music Core</span>'
             
             ]
     },
@@ -208,7 +257,11 @@ let gameArr = [
             'Runaway',
             '1N4U',
             'MLK',
-            'Unstopable'
+            'Unstopable',
+            'FutuRe',
+            '4EVER',
+            'Kross',
+            'NXT'
             
             ]
     },
@@ -222,7 +275,13 @@ let gameArr = [
             'Wanna One - Energetic',
             'Pentagon - Gorilla',
             'NU`EST - Face',
-            'KNK - Knock'
+            'KNK - Knock',
+            'ENHYPEN - Given-Taken',
+            'Shinee - Replay',
+            'TXT - Crown',
+            'ATEEZ - Treasure',
+            'Stray Kids - District 9',
+            'Monsta X - Trespass'
             ]
     }
 
